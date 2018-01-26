364771c77a539dbd985961496df88698e950ee98
madhanmanilalap <35652995+madhanmanilalap@users.noreply.github.com>
2018-01-26 16:33:20 +0530

    Create mm.c

---
+++ mm.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c[50],d,i;
+printf("Enter the values of a b ");
+scanf("%d%d",&a,&b);
+d=0;
+for(i=0;i<a;a++)
+d+=a;
+for(i=0;i<b;b++)
+d+c[i];
+printf("sum is %d",d+c[i]);
+getch();
+}
